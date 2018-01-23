d54bd7c27c5293b7980a788aad195f26191a4cfc
sagewabi <ahmedlelamo1@gmail.com>
2018-01-23 01:37:03 -0700

    add countPrime

---
+++ countPrimes/countPrimes.go
@@ -0,0 +1,25 @@
+package countPrimes
+
+// return list of primes less than N
+func countPrimes(N int) (n int) {
+
+	var primes []int
+
+	b := make([]bool, N)
+
+	for i := 2; i < N; i++ {
+		if b[i] == true {
+			continue
+		}
+		primes = append(primes, i)
+
+		for k := i * i; k < N; k += i {
+			b[k] = true
+
+		}
+
+	}
+	n = len(primes)
+
+	return
+}
